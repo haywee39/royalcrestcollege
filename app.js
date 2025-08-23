d09cab34992bc8a44275a325e86f6790856c76a8
Your Name <[haywee39@gmail.com]>
2025-08-23 15:51:30 +0100

    loaedr for home abt bgins

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// LOADER 
+    let percent = 0;
+    let percentageText = document.getElementById("percentage");
+    let progressBarFill = document.querySelector(".progress-bar-fill");
+    let loader = document.getElementById("loader");
+    let content = document.getElementById("content");
+
+    let interval = setInterval(() => {
+      percent++;
+      percentageText.textContent = percent + "%";
+      progressBarFill.style.width = percent + "%";
+
+      if (percent >= 100) {
+        clearInterval(interval);
+        loader.classList.add("fade-out");
+        setTimeout(() => {
+          loader.style.display = "none";
+          content.style.display = "block";
+        }, 800);
+      }
+    }, 30); // Adjust speed (30ms × 100 = 3 seconds total)
+  // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+
 // FULLSCREEN MENU DESIGN
   const menuToggle = document.getElementById("menuToggle");
   const fullscreenNav = document.getElementById("fullscreenNav");
