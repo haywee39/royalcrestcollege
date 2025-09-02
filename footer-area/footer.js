aaf399885dde6b1ff1c75025bd169c7cedd055fb
Your Name <[haywee39@gmail.com]>
2025-09-02 20:43:52 +0100

    test new footer on abt page

--- footer-area/footer.js
+++ footer-area/footer.js
@@ -1,5 +1,5 @@
 // Load footer.html into the #footer div
-fetch("/footer-nav/footer.html")
+fetch("/footer-area/footer.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
