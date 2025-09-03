fb433ee1b4dff3f3954420acf51630552a2ad535
Your Name <[haywee39@gmail.com]>
2025-09-03 04:00:00 +0100

    footerverror fixing across all pgs

--- footer-area/footer.js
+++
@@ -1,7 +0,0 @@
-// Load footer.html into the #footer div
-fetch("/footer-area/footer.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("footer-page").innerHTML = data;
-  })
-  .catch(error => console.error("Error loading footer-page:", error));
